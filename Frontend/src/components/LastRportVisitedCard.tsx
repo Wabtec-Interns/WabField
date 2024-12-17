e94c595981937caaf8b5358444dd2326363373d1
darlanZero <darliankeira229@gmail.com>
2024-12-17 17:46:44 +0000

    finalized with Related Projects Card

--- Frontend/src/components/LastRportVisitedCard.tsx
+++ Frontend/src/components/LastRportVisitedCard.tsx
@@ -1,14 +1,20 @@
 import { Badge, Box, Card, Collapsible, Image } from "@chakra-ui/react"
 import { mockProject, mockReport } from "../assets/mockData"
+import { ArrowBigRightDash } from "lucide-react"
 
 
 const LastReportVisitedCard = () => {
   return (
     <>
         <section className="flex justify-center items-center text-center p-2 gap-2">
-           <Collapsible.Root>
-                <Collapsible.Trigger>
-                    <h1 className="text-2xl">Last report visited</h1>
+           <Collapsible.Root defaultOpen>
+                <Collapsible.Trigger 
+                    
+                >
+                    <div className="RelatedProjectsCardHomepageTitle p-2 gap-2">
+                        <ArrowBigRightDash className="text-orange-50" />
+                        <h1 className="text-2xl text-orange-50 capitalize">Último relatório visitado</h1>
+                    </div>
                 </Collapsible.Trigger>
                 <Collapsible.Content>
                     <Box
@@ -42,7 +48,7 @@ const LastReportVisitedCard = () => {
                                 />
 
                                 <Box>
-                                    <Card.Body>
+                                    <Card.Body className="text-white">
                                         <Card.Title>{mockReport.reportTitle}</Card.Title>
                                         <Card.Description>{mockReport.reportDescription}</Card.Description>
                                         <Card.Footer mt={'4'}>
@@ -63,12 +69,15 @@ const LastReportVisitedCard = () => {
         <section className="flex justify-center items-center text-center p-2 gap-2">
             <Collapsible.Root>
                 <Collapsible.Trigger>
-                    <h1 className="text-2xl">From the related project</h1>
+                    <div className="RelatedProjectsCardHomepageTitle p-2 gap-2">
+                        <ArrowBigRightDash className="text-orange-50" />
+                        <h1 className="text-2xl capitalize">Do projeto relacionado</h1>
+                    </div>
                 </Collapsible.Trigger>
 
                 <Collapsible.Content>
                     <Box
-                        padding={4}
+                        padding={2}
                         borderRadius="md"
                         borderWidth={1}
                         boxShadow="lg"
@@ -76,17 +85,50 @@ const LastReportVisitedCard = () => {
                         shadow={'md'} 
                     >
                         <div className="flex flex-col gap-2">
-                            <img src={mockProject.projectImage} alt="Project" className="w-1/2 h-1/2" />
-                            <h1 className="text-2xl">{mockProject.projectName}</h1>
-                            <p className="flex flex-col gap-2 text-lg">{mockProject.projectDescription}</p>
-                            <p className="flex flex-col gap-2 text-lg">Start Date: {mockProject.projectStartDate}</p>
-                            <p className="flex flex-col gap-2 text-lg">End Date: {mockProject.projectEndDate}</p>
-                            <p className="flex flex-col gap-2 text-lg">Status: {mockProject.projectStatus}</p>
-                            <p className="flex flex-col gap-2 text-lg">Period: {mockProject.projectPeriod}</p>
-                            <p className="flex flex-col gap-2 text-lg">Report Summary: {mockProject.reportSummary}</p>
-                            <p className="flex flex-col gap-2 text-lg">{mockProject.employedCompany}</p>
-                            <p className="flex flex-col gap-2 text-lg">{mockProject.intermediaryCompany}</p>
-                            <p className="flex flex-col gap-2 text-lg">{mockProject.clientCompany}</p>
+                            <Card.Root
+                                flexDirection={'row'}
+                                justifyContent={'space-between'}
+                                alignItems={'center'}
+                                borderWidth={1}
+                                borderRadius={'md'}
+                                boxShadow={'md'}
+                                padding={4}
+                                bg={'blackAlpha.600'}
+                                shadow={'md'}
+                                maxW={'fit'}
+                            >
+                                <Image 
+                                    src={mockProject.projectImage}
+                                    alt="Project"
+                                    objectFit={'cover'}
+                                    borderRadius="md"
+                                    boxShadow="md"
+                                    maxW={'150px'}
+                                />
+
+                                <Box>
+                                    <Card.Body 
+                                        className="text-white"
+                                    >
+                                        <Card.Title>{mockProject.projectName}</Card.Title>
+                                        <Card.Description>{mockProject.projectDescription}</Card.Description>
+                                        <Card.Footer mt={'4'}>
+                                            <div
+                                                className="flex flex-col gap-2"
+                                            >
+                                            <Badge>Start date: {mockProject.projectStartDate}</Badge>
+                                            <Badge>End date: {mockProject.projectEndDate}</Badge>
+                                            <Badge>Status: {mockProject.projectStatus}</Badge>
+                                            <Badge>Period: {mockProject.projectPeriod}</Badge>
+                                            <Badge>Report Summary: {mockProject.reportSummary}</Badge>
+                                            <Badge>Employed Company: {mockProject.employedCompany}</Badge> 
+                                            <Badge>Intermediary Company: {mockProject.intermediaryCompany}</Badge>
+                                            <Badge>Client Company: {mockProject.clientCompany}</Badge>
+                                            </div>
+                                        </Card.Footer>
+                                    </Card.Body>
+                                </Box>
+                            </Card.Root>
                         </div>
                     </Box>
                 </Collapsible.Content>
