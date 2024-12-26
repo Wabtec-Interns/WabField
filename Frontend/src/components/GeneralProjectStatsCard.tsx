24e596f1cedf6fe8753d957a70f1605c3eb5c99e
darlanZero <darliankeira229@gmail.com>
2024-12-26 13:02:04 +0000

    trying to fix UI error

--- Frontend/src/components/GeneralProjectStatsCard.tsx
+++ Frontend/src/components/GeneralProjectStatsCard.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-empty-object-type */
-import { Badge, Box, HStack, Image, VStack } from '@chakra-ui/react'
+import { Badge, Box, HStack, Image, SimpleGrid, Tabs, TabsContent } from '@chakra-ui/react'
 import { PureComponent } from 'react'
 import { Pie, PieChart, ResponsiveContainer, Sector, PieProps } from 'recharts'
 import WabtecLogo from '../assets/Wabtec_Corporation-Logo.wine.svg'
@@ -95,54 +95,128 @@ class GeneralProjectStatsCard extends PureComponent<{}, GeneralProjectStatsCardS
     render() {
 
         return (
-          <Box w={'100%'} h={'800px'} borderWidth={'1px'} borderRadius={'md'} >
-            
-            <HStack w={'100%'} h={'100%'} justifyContent='center' alignItems='center'>
-              <VStack p='4' alignItems='flex-start' spaceY='4'>
-                <Box w='400px' h='200px' bg='gray.200' borderRadius='md'>
-                  <Image src={CardData.imageUrl} className='w-full h-full' alt='Wabtec' />
-                </Box>
-                <Box fontSize='lg' fontWeight='bold' color='black'>
-                  {CardData.title}
-                </Box>
-                <Box fontSize='sm' color='gray.500' w={'400px'}>
-                  {CardData.description}
-                </Box>
-              </VStack> 
-              <ResponsiveContainer width={600} height={600}>
-                <PieChart width={600} height={600}>
-                  <Pie 
-                    data={PieChartdata} 
-                    activeIndex={this.state.activeIndex}
-                    activeShape={renderActiveShapeWrapper}
-                    onMouseEnter={this.onPieEnter}
-                    dataKey="value" 
-                    nameKey="name" 
-                    cx="50%" 
-                    cy="50%" 
-                    innerRadius={60} 
-                    outerRadius={80} 
-                    fill='#8884d8' 
-                  />
-                </PieChart>
-              </ResponsiveContainer>
-            </HStack>
-            
-      
-            <Box
-                p='4' 
-                spaceY='2'
-    
+          <>
+          
+            <Tabs.Root
+              key={'tab'}
+              defaultValue={'Description'}
+              variant={'enclosed'}
+              rounded={'2xl'}
+              textAlign={'center'}
+              alignItems={'center'}
+              justifyContent={'center'}
             >
-                <HStack>
-                    {PieChartdata.map((data, index) => (
-                      <Badge key={index} colorPalette={getColorPallete(data.name)}>
-                        Relatórios {data.name} : {data.value}
-                      </Badge>
-                    ))}
-                </HStack>
-            </Box>
-          </Box>
+              <SimpleGrid
+                columns={1}
+                gap={2}
+                width={'full'}
+                p='4'
+                bg='white'
+                borderRadius='md'
+                borderWidth='1px'
+                textAlign='center'
+                alignItems='center'
+                justifyContent='center'
+              >
+              
+                <Tabs.List
+                  key={'tabList'}
+                  display={'flex'}
+                  flexDirection={'row'}
+                  justifyContent={'center'}
+                  p='2'
+                  gap={2}
+                  bg='red.800'
+                  color={'white'}
+                  alignContent={'center'}
+                  textAlign={'center'}  
+                  border={'2px solid'}
+                  borderColor={'gray.200'}
+                >
+                  <Tabs.Trigger
+                    value='Description'
+                    _active={{ bg: 'red.600', rounded: '2xl' }}
+                    _focus={{ bg: 'red.600', rounded: '2xl' }}
+                    _hover={{ bg: 'red.600', rounded: '2xl' }}
+                    animation={'scale-in-center 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both'}
+                    transition={'all 0.25s'}
+                    className='gap-2 p-2' 
+
+                  >
+                    Descrição Geral
+                  </Tabs.Trigger>
+
+                  <Tabs.Trigger
+                    value='Dashboard'
+                    className='gap-2 p-2'
+                    _active={{ bg: 'red.600', rounded: '2xl' }}
+                    _focus={{ bg: 'red.600', rounded: '2xl' }}
+                    _hover={{ bg: 'red.600', rounded: '2xl' }}
+                    animation={'scale-in-center 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both'}
+                    transition={'all 0.25s'}
+
+                  >
+                    Dashboard
+                  </Tabs.Trigger>
+                </Tabs.List>
+              
+            </SimpleGrid>
+
+            <Box w={'100%'} h={'800px'} borderWidth={'1px'} borderRadius={'md'} >
+              <Tabs.Content 
+                value='Description'
+              />
+                  <Box w='400px' h='200px' bg='gray.200' borderRadius='md'>
+                    <Image src={CardData.imageUrl} className='w-full h-full' alt='Wabtec' />
+                  </Box>
+                  <Box p='4' />
+                  <Box fontSize='lg' fontWeight='bold' color='black'>
+                    {CardData.title}
+                  </Box>
+                  <Box p='2' />
+                  <Box fontSize='sm' color='gray.500' w={'400px'}>
+                    {CardData.description}
+                  </Box>
+                </Box>
+                
+              </Tabs.Content>
+              
+              <Tabs.Content
+                value='Dashboard'
+              >
+                <ResponsiveContainer width={600} height={600}>
+                  <PieChart width={600} height={600}>
+                    <Pie 
+                      data={PieChartdata} 
+                      activeIndex={this.state.activeIndex}
+                      activeShape={renderActiveShapeWrapper}
+                      onMouseEnter={this.onPieEnter}
+                      dataKey="value" 
+                      nameKey="name" 
+                      cx="50%" 
+                      cy="50%" 
+                      innerRadius={60} 
+                      outerRadius={80} 
+                      fill='#8884d8' 
+                    />
+                  </PieChart>
+                </ResponsiveContainer>
+        
+                <Box
+                    p='4' 
+                    spaceY='2'
+                >
+                    <HStack>
+                        {PieChartdata.map((data, index) => (
+                          <Badge key={index} colorPalette={getColorPallete(data.name)}>
+                            Relatórios {data.name} : {data.value}
+                          </Badge>
+                        ))}
+                    </HStack>
+                </Box>
+              </Tabs.Content>
+            </Tabs.Root>
+          </>
         )
     }
 
