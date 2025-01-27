0563df5704ae17061f30c4a570efc5fa6b7b6af9
darlanZero <darliankeira229@gmail.com>
2025-01-27 17:57:03 +0000

    CSS fix up to date

--- Frontend/src/pages/LoggedIn/Home.tsx
+++ Frontend/src/pages/LoggedIn/Home.tsx
@@ -4,12 +4,12 @@ import GeneralProjectStatsCard from "../../components/GeneralProjectStatsCard";
 export default function Home() {
   return (
     <>
-      <div className="flex h-full w-full p-2 gap-2">
+      <div className="flex flex-row  p-2 gap-2">
         <section className="w-1/3 h-full bg-red-800 rounded-md">
           <LastReportVisitedCard />
         </section>
 
-        <section className="w-2/3 h-full gap-2 p-2 bg-gray-300 rounded-md">
+        <section className="w-2/3 h-full  gap-2 p-2 bg-gray-300 rounded-md">
           <GeneralProjectStatsCard />
         </section>
       </div>
