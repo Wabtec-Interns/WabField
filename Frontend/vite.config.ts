bf845bcc8d86ae13290865c8e30b5e9b01b2698a
darlanZero <darliankeira229@gmail.com>
2025-02-26 17:31:37 +0000

    fix host

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -6,6 +6,6 @@ import tsConfigPaths from 'vite-tsconfig-paths'
 export default defineConfig({
   plugins: [react(), tsConfigPaths()],
   server: {
-    allowedHosts: ['localhost', 'IP_ADDRESS', 'k4ubbvkq-6gqwmhk4-2nzaf6pafhd9.acb2-preview.marscode.dev'],
+    host: '0.0.0.0',
   }
 })
