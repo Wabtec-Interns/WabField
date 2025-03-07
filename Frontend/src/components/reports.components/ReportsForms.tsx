1df7e22c047eb5dbf748bc74f0e0f150f0fa0c0b
maxwillian29 <maxwilliantelmo@gmail.com>
2025-03-07 14:19:35 -0300

    More Functions ReportForms

--- Frontend/src/components/reports.components/ReportsForms.tsx
+++ Frontend/src/components/reports.components/ReportsForms.tsx
@@ -1,10 +1,21 @@
 import { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { getValueByDataKey } from "recharts/types/util/ChartUtils";
+import { Label } from "recharts";
 
 
 type FormData = {
   status: string;
-  Type: string;
+  type: string;
+  morningType: string;
+  morningWeather: string;
+  morningWork: string;
+  afternoonType: string;
+  afternoonWeather: string;
+  afternoonWork: string;
+  nightType: string;
+  nightWeather: string;
+  nightWork: string;
   weatherCondition: string;
   workCondition: string;
   profissionalResponsable: string;
@@ -44,7 +55,16 @@ const PROFISSIONAL_RESPONSABLE_CHOICHES = [
 const ReportForms = () => {
   const [formData, setFormData] = useState<FormData>({
     status: "",
-    Type: "",
+    type: "",
+    morningType: "",
+    morningWeather: "",
+    morningWork: "",
+    afternoonType: "",
+    afternoonWeather: "",
+    afternoonWork: "",
+    nightType: "",
+    nightWeather: "",
+    nightWork: "",
     weatherCondition: "",
     workCondition: "",
     profissionalResponsable: "",
@@ -70,75 +90,116 @@ const ReportForms = () => {
   };
 
   return (
-    <div className="container mt-5 text-light bg-dark m-2 rounded" style={{ maxWidth: "600px" }}>
-      <h1 className="text-left mb-4">Relatório de Campo</h1>
+
+
+    <div className="container mt-0 text-light bg-dark center rounded" style={{ maxWidth: "1500px"}}>
+      <h1 className="text-center mb-4">Relatório de Campo</h1>
       <form onSubmit={handleSubmit}>
-        <div className="mb-3">
-          <label className="form-label">Status</label>
-          <select className="form-select" name="status" value={formData.status} onChange={handleChange} required>
-            <option value=""> </option>
-            {REPORT_STATUS_CHOICES.map(([value, label]) => (
-              <option key={value} value={value}>{label}</option>
-            ))}
-          </select>
-        </div>
+      <div className="text-center mb-4" style={{display: 'flex', justifyContent: 'space-between', padding: '20px', border: '1px  #000' }}>
 
-        <div className="mb-3">
-          <label className="form-label">Tipo</label>
-          <select className="form-select" name="Type" value={formData.Type} onChange={handleChange} required>
-            <option value=""> </option>
-            {TYPE_REPORT_CHOICES.map(([value, label]) => (
-              <option key={value} value={value}>{label}</option>
-            ))}
-          </select>
-        </div>
+      <div className="mb-3 text-left ml-1" style={{ flex: 1, margin: '0 10px', padding: '20px', border: '1px solid #000', textAlign: 'center' }}>
+        <div className= "text-left mb-6" style={{textAlign: 'center'}}>Turno manhã:</div>
+          <div style={{textAlign: "left"}}>Tipo de registro:</div>
+            <select className="form-select" name="morningType" value={formData.morningType} onChange={handleChange} required>
+              <label className="form-label"></label>
+                <option value=""></option>
+                {TYPE_REPORT_CHOICES.map(([value, label]) => (
+                <option key={value} value={value}>{label}</option>
+          ))}
+            </select>
+            <div style={{textAlign: 'left'}}>Condições climáticas:</div>
+            <select className="form-select" name="morningWeather" value={formData.morningWeather} onChange={handleChange} required>
+              <label className="form-label"></label>
+                <option value=""></option>
+                {WEATHER_CONDITION_CHOICES.map(([value, label]) => (
+                <option key={value} value={value}>{label}</option>
+          ))}
+            </select>
+            <div style={{textAlign: 'left'}}>Condições de trabalho:</div>
+            <select className="form-select" name="morningWork" value={formData.morningWork} onChange={handleChange} required>
+              <label className="form-label"></label>
+              <option value=""></option>
+              {WORK_CONDITION_CHOICES.map(([value, label]) => (
+                <option key={value} value={value}>{label}</option>
+              ))}
+            </select>
+      </div>
+    
 
-        <div className="mb-3">
-          <label className="form-label">Condição Climática</label>
-          <select className="form-select" name="weatherCondition" value={formData.weatherCondition} onChange={handleChange} required>
-            <option value=""> </option>
-            {WEATHER_CONDITION_CHOICES.map(([value, label]) => (
-              <option key={value} value={value}>{label}</option>
-            ))}
-          </select>
-        </div>
+      <div className="mb-3 text-left ml-1" style={{ flex: 1, margin: '0 10px', padding: '20px', border: '1px solid #000', textAlign: 'center' }}>
+        <div className= "text-left mb-6" style={{textAlign: 'center'}}>Turno tarde:</div>
+          <div style={{textAlign: "left"}}>Tipo de registro:</div>
+            <select className="form-select" name="afternoonType" value={formData.afternoonType} onChange={handleChange} required>
+              <label className="form-label"></label>
+                <option value=""></option>
+                {TYPE_REPORT_CHOICES.map(([value, label]) => (
+                <option key={value} value={value}>{label}</option>
+          ))}
+            </select>
+            <div style={{textAlign: 'left'}}>Condições climáticas:</div>
+            <select className="form-select" name="afternoonWeather" value={formData.afternoonWeather} onChange={handleChange} required>
+              <label className="form-label"></label>
+                <option value=""></option>
+                {WEATHER_CONDITION_CHOICES.map(([value, label]) => (
+                <option key={value} value={value}>{label}</option>
+          ))}
+            </select>
+            <div style={{textAlign: 'left'}}>Condições de trabalho:</div>
+            <select className="form-select" name="afternoonWork" value={formData.afternoonWork} onChange={handleChange} required>
+              <label className="form-label"></label>
+              <option value=""></option>
+              {WORK_CONDITION_CHOICES.map(([value, label]) => (
+                <option key={value} value={value}>{label}</option>
+              ))}
+            </select>            
+      </div>
 
-        <div className="mb-3">
-          <label className="form-label">Condição de Trabalho</label>
-          <select className="form-select" name="workCondition" value={formData.workCondition} onChange={handleChange} required>
-            <option value=""> </option>
-            {WORK_CONDITION_CHOICES.map(([value, label]) => (
-              <option key={value} value={value}>{label}</option>
-            ))}
-          </select>
-        </div>
-        <div className="mb-3">
-            <label className="form-label">Qual Pátio</label>
-            <input type="text" className="form-control" name="otherField1" placeholder="Insira seu texto aqui" />
-        </div>
-        <div className="mb-3">
-            <label className="form-label">Nome do profissional</label>
-            <select className="form-select" name="profissionalResponsable" value={formData.profissionalResponsable} onChange={handleChange} required>
-            <option value=""> </option>
-            {PROFISSIONAL_RESPONSABLE_CHOICHES.map(([value, label]) => (
-              <option key={value} value={value}>{label}</option>
-            ))}
-          </select>
-        </div>
-        <div className="mb-3">
-            <label className="form=label">Tarefas realizadas</label>
-            <input type="file"></input>
-        </div>
-      
+      <div className="mb-3 text-left ml-1" style={{ flex: 1, margin: '0 10px', padding: '20px', border: '1px solid #000', textAlign: 'center' }}>
+        <div className= "text-left mb-6" style={{textAlign: 'center'}}>Turno noite:</div>
+          <div style={{textAlign: "left"}}>Tipo de registro:</div>
+            <select className="form-select" name="nightType" value={formData.nightType} onChange={handleChange} required>
+              <label className="form-label"></label>
+                <option value=""></option>
+                {TYPE_REPORT_CHOICES.map(([value, label]) => (
+                <option key={value} value={value}>{label}</option>
+          ))}
+            </select>
+            <div style={{textAlign: 'left'}}>Condições climáticas:</div>
+            <select className="form-select" name="nightWeather" value={formData.nightWeather} onChange={handleChange} required>
+              <label className="form-label"></label>
+                <option value=""></option>
+                {WEATHER_CONDITION_CHOICES.map(([value, label]) => (
+                <option key={value} value={value}>{label}</option>
+          ))}
+            </select>
+            <div style={{textAlign: 'left'}}>Condições de trabalho:</div>
+            <select className="form-select" name="nightWork" value={formData.nightWork} onChange={handleChange} required>
+              <label className="form-label"></label>
+              <option value=""></option>
+              {WORK_CONDITION_CHOICES.map(([value, label]) => (
+                <option key={value} value={value}>{label}</option>
+              ))}
+            </select>
+      </div>
+
+    </div>
+
+      <div style={{ maxWidth: "1500px", margin: '0 10px', padding: '20px', border: '1px solid #ffffff' }}>
+        <div className="text-center mb-4" style={{ maxWidth: "1500px", border: '1px solid #000'}}>DIV para soltar os cards</div>
+        <div className="text-center mb-4" style={{ maxWidth: "1500px", border: '1px solid #000'}}>DIV para definir o tipo de servico ind, dir, ter</div>
+        <div className="text-center mb-4" style={{ maxWidth: "1500px", border: '1px solid #000'}}>DIV cards drag drop</div>
+      </div>
 
   
 
-        <button type="submit" className="btn btn-primary w-100 mt-3">
+      <button type="submit" className="btn btn-primary w-100 mt-3">
           Enviar
         </button>
       </form>
     </div>
+    
   );
 };
 
+//colocar a regra para tornar 1 das 3 div sobre turno obrigatórios* melhoria 1 
 export default ReportForms;
