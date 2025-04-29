3b8936b2ab3535852ac95c71484278ca1dbd2920
maxwillian29 <maxwilliantelmo@gmail.com>
2025-04-29 08:51:39 -0300

    minor ui changes

--- Frontend/src/components/ReportForms/ReportForms.tsx
+++ Frontend/src/components/ReportForms/ReportForms.tsx
@@ -133,6 +133,7 @@ const updateFormDataWithContracts = (contracts) => {
         
         <button type="submit" className="btn btn-primary w-100 mt-3">Enviar</button>
       </form>
+      <div></div>
     </div>
     
     
